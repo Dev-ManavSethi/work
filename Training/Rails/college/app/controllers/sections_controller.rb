0a602e952188f62b5271fbbf2fd5be5a2dc0df35
Manav Sethi <dev.manav.sethi@gmail.com>
2019-09-09 13:04:42 +0530

    added section controllers, modified departments_controller

--- Training/Rails/college/app/controllers/sections_controller.rb
+++ Training/Rails/college/app/controllers/sections_controller.rb
@@ -1,5 +1,37 @@
 class SectionsController < ActionController
 
+  def index
+    render 'sections/index'
+  end
 
+  def create
+    @section = Section.new(get_params)
+    @section.save
+    redirect_to
+  end
+
+  def new
+    render 'sections/new'
+  end
+
+  def edit
+    render 'sections/edit'
+  end
+
+  def show
+    render 'sections/show'
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def get_params
+    params.require(:section).permit(:id, :departments_id, :teachers_id)
+  end
 
 end

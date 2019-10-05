5ecf30755c2fa87cb3b80567fc3f44fad6b4d321
Manav Sethi <dev.manav.sethi@gmail.com>
2019-10-05 18:21:51 +0530

    added assinment

--- Training/Rails/college/app/controllers/user_controller.rb
+++ Training/Rails/college/app/controllers/user_controller.rb
@@ -1,10 +1,17 @@
 class UserController < ApplicationController
 
+    before_action :to_root, if: -> {user_signed_in?}
+
     def new
         @user = User.new
     end   
 
     def login
         @user = User.new
+    end
+
+    private
+    def to_root
+        redirect_to root_path
     end
 end

9cd15f68d1d03fd9670796095509a06463e4c8cf
Manav Sethi <dev.manav.sethi@gmail.com>
2019-10-14 16:51:55 +0530

    added react-rails with webpacker

--- Training/Rails/college/app/controllers/application_controller.rb
+++ Training/Rails/college/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require "google/cloud/translate"
+require "react-rails"
 
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.

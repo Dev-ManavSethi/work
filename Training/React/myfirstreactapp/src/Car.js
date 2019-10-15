6219e54a916c4df77f63e0a5b0ed921068be97c2
Manav Sethi <dev.manav.sethi@gmail.com>
2019-10-15 10:21:58 +0530

    added new react_on_rails app

--- Training/React/myfirstreactapp/src/Car.js
+++ Training/React/myfirstreactapp/src/Car.js
@@ -1,22 +1,24 @@
 import React from 'react';
 
 class Car extends React.Component{
+
+
     constructor(props){
         super(props);
-        this.brand = props.brand;
-        this.model = props.model;
-        this.year = props.year;
+        this.state = {brand: props.brand, model: props.model, year: props.year};
     }
 
     static getDerivedStateFromProps(props, state){
-        return {brand: props.brand, model: props.model, year: props.year};
+        //return {brand: props.brand, model: props.model, year: props.year}; //makes Car immutable
     }
 
     render(){
         return(<div>
-            <h1>Brand: {this.brand}</h1>
-            <h1>model: {this.model}</h1>
-            <h1>Year: {this.year}</h1>
+            <h1>Brand: {this.state.brand}</h1>
+            <button type="button" onClick={this.changeBrand}>Change Brand</button>
+            <h1>model: {this.state.model}</h1>
+            <h1>Year: {this.state.year}</h1>
+            
             </div>
             );
     }
@@ -24,19 +26,21 @@ class Car extends React.Component{
     componentDidMount(){
         setTimeout(() => {
             this.setState({brand: "New Brand"});
-            this.brand = "New Brand";
-            console.log(this.brand);
           }, 2000);
           setTimeout(() => {
             this.setState({model: "New Model"});
-            this.model = "New Model";
-            console.log(this.model);
           }, 4000);
           setTimeout(() => {
             this.setState({year: "New Year"});
-            this.year = 2002;
-            console.log(this.year);
           }, 6000);
+    }
+
+    shouldComponentUpdate() {
+        return true;
+      }
+
+    changeBrand = () => {
+        this.setState({brand: "New Brandddd"});
     }
 }
 

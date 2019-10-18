281fbb77d94c0f953b708c401dcefd09ba66e3df
Manav Sethi <dev.manav.sethi@gmail.com>
2019-10-18 19:13:39 +0530

    beautified code

--- Training/Rails/api-app/app/controllers/api/v1/companies_controller.rb
+++ Training/Rails/api-app/app/controllers/api/v1/companies_controller.rb
@@ -1,48 +1,59 @@
 module Api
-    module V1
-      class CompaniesController < ApplicationController
-        def index
-        end
+  module V1
+    class CompaniesController < ApplicationController
+      def index
+      end
 
-        def show
-        end
+      def show
+      end
+
         def create
+          
+          status = 0
+          message = ""
+          data = nil
+
           name = params[:name]
+
           uuid = UUID.new
           key = uuid.generate
+          
           company = Company.new(name:name, key:key)
-          response = APIresponse.new
+          
+          if company.save
+            api_key = ApiKey.new(key: key, company_id: company.id)
+            
+            if api_key.save
+              status = 201
+              message = "Company signup success"
+              data = company
 
-    if company.save
+            elsif !api_key.save
+              status = 500
+              message = api_key.errors.full_messages[0]
+              data = nil
+            end
 
-      api_key = ApiKey.new(key: key, company_id: company.id)
-      if api_key.save
-        response.status = 201
-        response.message = "Company signup success"
-        response.data = company
+          elsif !company.save
+            message = company.errors.full_messages[0]
+            case message
+            when "Name has already been taken"
+              status = 400
+              data = nil
+            else 
+              status = 500
+              data = nil
+            end
+          end
 
-      elsif !api_key.save
-        response.status = 500
-        response.message = api_key.errors.full_messages[0]
-        response.data = nil
+          API_respond(status, message, data)
+        end
+
+      def update
       end
-    elsif !company.save
-      response.message = company.errors.full_messages[0]
-      case response.message
-      when "Name has already been taken"
-        response.status = 400
+
+      def destroy
       end
-      response.data = nil
     end
-
-    render json: response.to_json, status: response.status
   end
-
-  def update
-  end
-
-  def destroy
-  end
-end
-end
 end
--- Training/Rails/api-app/app/controllers/application_controller.rb
+++ Training/Rails/api-app/app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::API
   APIresponse = Struct.new(:status, :message ,:data)
+
+  def API_respond(status, message, data)
+    response = APIresponse.new(status:status, message:message, data:data)
+    render response.to_json, status:status
+  end
 end
